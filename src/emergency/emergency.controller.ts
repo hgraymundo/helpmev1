c24101f0830f36c0761d262e786b4feecd595676
JRH <jrh@MacBook-Pro-de-JRH.local>
2024-05-02 19:51:41 -0600

    3

--- src/emergency/emergency.controller.ts
+++ src/emergency/emergency.controller.ts
@@ -42,14 +42,12 @@ export class EmergencyController {
         console.log(createEmergency.lon);
         console.log(__contacts);
         this.sendSMS(name, __contacts.substring(0, __contacts.length - 1), createEmergency.lat, createEmergency.lon);
+        this.getTokenTelegram( name, createEmergency.lat, createEmergency.lon);
       })
 
-   
-      
       return data;
     })
 
-      
    return data;
 
   }
@@ -88,6 +86,46 @@ export class EmergencyController {
       this.httpService.post('https://api.smsmasivos.com.mx/sms/send', data, { headers: headers } ).subscribe(res => {
          console.log(res);
       })
+  }
+
+  getTokenTelegram( name: string, lat: string, lon: string) {
+
+    const headers = {
+      'Content-Type': 'application/json'
+    }
+
+    let data = JSON.stringify({
+      "query": "query getTk{ getTk(email:\"db8481d1-aa14-b57a315472a9\", password:\"Qet1P$0!=fs\"){ status message token }}"
+    });
+
+    this.httpService.post('https://api2.idconnect.com.mx/graphql', data, { headers: headers } ).subscribe(res => {
+      
+        console.log(res.data.data.getTk.token);
+        const token = res.data.data.getTk.token;
+
+        const headers = { 
+          'Authorization': token, 
+          'Content-Type': 'application/json'
+        }
+        const message = " - localizado en: https://www.google.com/maps/search/?api=1&query=" + lat + "," + lon+ "&z=16"
+
+        let data = JSON.stringify({
+          query: `mutation setMensajeTelegram($Remitente:String!, $Mensaje:String!) {
+          setMensajeTelegram(Remitente:$Remitente, Mensaje:$Mensaje) {
+            status
+            message
+        
+          }
+        }`,
+          variables: { "Remitente":name, "Mensaje": message} 
+        });
+
+        this.httpService.post('https://api2.idconnect.com.mx/graphql', data, { headers: headers } ).subscribe(res => {
+          console.log(res.data);
+        })
+        
+    })
+    
   }
 
   // @Patch(':id')
